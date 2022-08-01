c30bd97ba76e0aa7062436923b839a24ce939963
Davidsonlinhss <davidson.linhares@outlook.com>
2022-08-01 20:42:39 -0300

    Atualização do formatador, removendo o :.5.2f e mantendo apenas a formatação :.2f

--- Exercício 4.10.py
+++ Exercício 4.10.py
@@ -5,36 +5,37 @@ Calcule o valor a pagar de acordo com a tabela a seguir:
 '''
 
 # Dados fornecidos pelos usuários
-consumo = input('Digite o consumo em kWh:\n')
-tipo_instalação = input('Informe o tipo de instalação (R) para residencial, (C) para comercial e (I) para industrial:\n')
+while True:
+    consumo = input('Digite o consumo em kWh:\n')
+    tipo_instalação = input('Informe o tipo de instalação (R) para residencial, (C) para comercial e (I) para industrial:\n')
 
-if consumo.isdigit() and tipo_instalação.isupper():  # Verificador
-    consumo = float(consumo)  # Convertendo o tipo de dados após a verificador
-    message = 'O valor a ser pago pelo consumo será de'  # String para substituir nas mensagens
+    if consumo.isdigit() and tipo_instalação.isupper():  # Verificador
+        consumo = float(consumo)  # Convertendo o tipo de dados após a verificador
+        message = 'O valor a ser pago pelo consumo será de'  # String para substituir nas mensagens
 
-    if tipo_instalação == 'R' and consumo <= 500:
-        consumo_calc = consumo * 0.40
-        print(f'{message} R${consumo_calc:.2f}.')
+        if tipo_instalação == 'R' and consumo <= 500:
+            consumo_calc = consumo * 0.40
+            print(f'{message} R${consumo_calc:.2f}.')
 
-    elif tipo_instalação == 'R' and consumo > 500:
-        consumo_calc = consumo * 0.65
-        print(f'{message} R${consumo_calc:.2f}.')
+        elif tipo_instalação == 'R' and consumo > 500:
+            consumo_calc = consumo * 0.65
+            print(f'{message} R${consumo_calc:.2f}.')
 
-    elif tipo_instalação == 'C' and consumo <= 1000:
-        consumo_cal = consumo * 0.55
-        print(f'{message} R${consumo_cal:.2f}.')
+        elif tipo_instalação == 'C' and consumo <= 1000:
+            consumo_cal = consumo * 0.55
+            print(f'{message} R${consumo_cal:.2f}.')
 
-    elif tipo_instalação == 'C' and consumo > 1000:
-        consumo_cal = consumo * 0.60
-        print(f'{message} R${consumo_cal:.2f}.')
+        elif tipo_instalação == 'C' and consumo > 1000:
+            consumo_cal = consumo * 0.60
+            print(f'{message} R${consumo_cal:.2f}.')
 
-    elif tipo_instalação == 'I' and consumo <= 5000:
-        consumo_cal = consumo * 0.55
-        print(f'{message} R${consumo_cal:.2f}.')
+        elif tipo_instalação == 'I' and consumo <= 5000:
+            consumo_cal = consumo * 0.55
+            print(f'{message} R${consumo_cal:.2f}.')
+
+        else:
+            consumo_cal = consumo * 0.60
+            print(f'{message} R${consumo_cal:.2f}.')
 
     else:
-        consumo_cal = consumo * 0.60
-        print(f'{message} R${consumo_cal:.2f}.')
-
-else:
-    print('Digite os dados da forma que se pede.')  # Caso as entradas fornecidas não estejam de acordo
+        print('Digite os dados da forma que se pede.')  # Caso as entradas fornecidas não estejam de acordo
